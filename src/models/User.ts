baf48a67d64995413807bb3fbd3a869a9c5a890c
Harold Alcala <harold.dev@gmail.com>
2021-09-04 18:09:13 +0800

    user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,31 +1,44 @@
-import { model, Schema } from "mongoose"
+import { model, Schema } from "mongoose";
 
-const schema = new Schema<User>({
-    username:{type:String,require:true,min:3,max:20, unique:true},
-    name:{type:String, required:true},
-    email:{type:String, required:true},
-    isAdmin:{type:Boolean, required:true, default:false},
-    password:{type:String, required:true},
-    profilePicture:{type:String,},
-    converPicture:{type:String,},
-    followers:[ {
-        type: String,
-    }],
-    following:[{type:String}]
-},{timestamps:true})
+const schema = new Schema<User>(
+    {
+        username: { type: String, require: true, min: 3, max: 20, unique: true },
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        isAdmin: { type: Boolean, required: true, default: false },
+        password: { type: String, required: true },
+        profilePicture: { type: String },
+        converPicture: { type: String },
+        followers: [
+            {
+                type: String,
+            },
+        ],
+        following: [{ type: String }],
+        desc: { type: String, required: true },
+        city: { type: String, required: true },
+        hometown: { type: String, required: true },
+        relationship: { type: String, required: true },
+    },
+    { timestamps: true }
+);
 
 export interface User {
-    username:string
-    name:string
-    email:string
-    isAdmin:boolean
-    password:string
-    profilePicture?:string
-    converPicture:string
-    followers:string[]
-    following:string[]
+    username: string;
+    name: string;
+    email: string;
+    isAdmin: boolean;
+    password: string;
+    profilePicture?: string;
+    converPicture: string;
+    followers: string[];
+    following: string[];
+    desc: string;
+    city: string;
+    hometown: string;
+    relationship: string;
 }
 
-export const UserModel=model<User>("Users",schema)
+export const UserModel = model<User>("Users", schema);
 
-export default UserModel
+export default UserModel;
